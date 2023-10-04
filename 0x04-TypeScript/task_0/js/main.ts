ec2d3041a2abade914cdd6bb999a1ae05b02b330
UmarMTukur <umartukur55@gmail.com>
2023-10-04 13:27:20 -0700

    Init

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,12 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "Zeliq",
+	lastName: "Shannon",
+	age: 40,
+	location: "Nakuru"
